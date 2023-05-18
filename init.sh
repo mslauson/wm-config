c62b236b9076419598a5a60c296769e56306e16f
Matthew Slauson <matthew@slauson.io>
2023-05-18 16:22:23 -0500

    add hypr

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 currentPath="$(pwd)"
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
-for dir in xmonad
+for dir in hypr xmonad
 do
     rm -rf ~/.config/$dir
     ln -s "$currentPath"/$dir ~/.config/$dir
