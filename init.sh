47986f66fc7d8a890da91a61631fa6c9c3411b96
Matthew Slauson <matthew@slauson.io>
2023-05-19 15:08:18 -0500

    awesome starting point

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 currentPath="$(pwd)"
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
-for dir in hypr xmonad
+for dir in bat btop cava dunst fish hypr kitty wofi
 do
     rm -rf ~/.config/$dir
     ln -s "$currentPath"/$dir ~/.config/$dir
 done
 
-for item in .xprofile
+for item in .xprofile .starship.toml
 do
   rm -rf ~/$item
 ln -s "$currentPath"/$item ~/$item
