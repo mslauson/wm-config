4f3e1e1d854bf8b5c5ff730f3349f003c88b04c5
Matthew Slauson <matthew@slauson.io>
2023-05-20 02:10:27 -0500

    fixes

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-yay -Syu hyprland-git waybar-hyprland-git cava waybar-mpris-git python rustup kitty fish wofi xdg-desktop-portal-hyprland-git tty-clock-git swaylockd grim slurp pokemon-colorscripts-git starship jq dunst wl-clipboard swaylock-effects-git swww-git
+yay -Syu hyprland waybar-hyprland cava waybar-mpris-git python rustup kitty fish wofi xdg-desktop-portal-hyprland-git tty-clock-git swaylockd grim slurp pokemon-colorscripts-git starship jq dunst wl-clipboard swaylock-effects-git swww-git
 
 currentPath="$(pwd)"
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
