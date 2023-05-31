bedfc692f6078fe511fc29a13cdeece4772bbd4a
Matthew Slauson <matthew@slauson.io>
2023-05-31 15:40:08 -0500

    remove

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 currentPath="$(pwd)"
+ 
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
 for dir in bat btop cava dunst fish hypr kitty wofi
 do
@@ -9,5 +10,15 @@ done
 for item in .xprofile .starship.toml
 do
   rm -rf ~/$item
-ln -s "$currentPath"/$item ~/$item
+  ln -s "$currentPath"/$item ~/$item
 done
+
+rm -rf ~/.config/hypr/store
+mkdir ~/.config/hypr/store
+touch ~/.config/hypr/store/dynamic_out.txt
+touch ~/.config/hypr/store/prev.txt
+touch ~/.config/hypr/store/latest_notif
+
+chmod +x ~/.config/hypr/scripts/tools/*
+chmod +x ~/.config/hypr/scripts/*
+chmod +x ~/.config/hypr/*
