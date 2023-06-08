75093cde333429ae92c40399859a2dc3010d46d1
Matthew Slauson <matthew@slauson.io>
2023-06-07 21:11:39 -0500

    fix sizing

--- hypr/scripts/open-windows.sh
+++ hypr/scripts/open-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parse the JSON and create a menu with wofi
-selection=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.name): \(.class) - \(.title)"' | wofi --show=dmenu)
+selection=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.name): \(.class) - \(.title)"' | wofi --show=dmenu -c ~/.config/wofi/openwindows)
 
 # Get the workspace name from the selection
 workspace=$(echo "$selection" | cut -d':' -f1)
