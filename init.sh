b144a8ffd7fe41ed923b70d5beee108475168650
Matthew Slauson <matthew@slauson.io>
2023-06-12 11:34:55 -0500

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-currentPath=$HOME
+currentPath=/home/mslauson/projects/config/wm-config
  
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
 for dir in bat btop cava dunst fish hypr kitty wofi
@@ -13,12 +13,12 @@ do
   ln -s "$currentPath"/$item ~/$item
 done
 
-rm -rf ~/.config/hypr/store
-mkdir ~/.config/hypr/store
-touch ~/.config/hypr/store/dynamic_out.txt
-touch ~/.config/hypr/store/prev.txt
-touch ~/.config/hypr/store/latest_notif
+rm -rf /home/mslauson/projects/config/wm-config/hypr/store
+mkdir /home/mslauson/projects/config/wm-config/hypr/store
+touch /home/mslauson/projects/config/wm-config/hypr/store/dynamic_out.txt
+touch /home/mslauson/projects/config/wm-config/hypr/store/prev.txt
+touch /home/mslauson/projects/config/wm-config/hypr/store/latest_notif
 
-chmod +x ~/.config/hypr/scripts/tools/*
-chmod +x ~/.config/hypr/scripts/*
-chmod +x ~/.config/hypr/*
+chmod +x /home/mslauson/projects/config/wm-config/hypr/scripts/tools/*
+chmod +x /home/mslauson/projects/config/wm-config/hypr/scripts/*
+chmod +x /home/mslauson/projects/config/wm-config/hypr/*
