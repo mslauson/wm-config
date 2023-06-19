93c295840d4f1dc9d9a239ce18ec1fae55884d3b
Matthew Slauson <matthew@slauson.io>
2023-06-19 10:14:30 -0500

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 currentPath=/home/mslauson/projects/config/wm-config
  
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
-for dir in bat btop cava dunst fish hypr kitty wofi
+for dir in bat btop cava dunst fish hypr kitty wofi scripts starship swaylock waybar wlogout networkmanager-dmenu
 do
     rm -rf ~/.config/$dir
     ln -s "$currentPath"/$dir ~/.config/$dir
