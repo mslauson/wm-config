03960565f7952d9727fa55a65e067161c93a5f95
Matthew Slauson <matthew@slauson.io>
2023-06-23 19:25:23 -0500

    fix

--- hypr/scripts/tmux_init.sh
+++ hypr/scripts/tmux_init.sh
@@ -21,17 +21,17 @@ tmux new-window -t $NOTES:0 -n 'notes'
 tmux send-keys -t $NOTES:0 'nvim -c ":Neorg index"' C-m
 
 tmux new-window -t $WORK:0 -n 'api'
-tmux send-keys -t $WORK:0 'pApi && nvim' C-m
+tmux send-keys -t $WORK:0 'pApi' C-m
 
 tmux new-window -t $WORK:1 -n 'ui'
-tmux send-keys -t $WORK:1 'pUi && nvim' C-m
+tmux send-keys -t $WORK:1 'pUi' C-m
 
 tmux new-window -t $CONF:0 -n 'nvim'
-tmux send-keys -t $CONF:0 'pConf && cd astro-config && nvim .' C-m
+tmux send-keys -t $CONF:0 'pConf && cd astro-config' C-m
 
 tmux new-window -t $CONF:1 -n 'wm'
-tmux send-keys -t $CONF:1 'pConf && cd wm-config && nvim .' C-m
+tmux send-keys -t $CONF:1 'pConf && cd wm-config' C-m
 
 tmux new-window -t $CONF:2 -n 'term'
-tmux send-keys -t $CONF:2 'pConf && cd term-config && nvim .' C-m
+tmux send-keys -t $CONF:2 'pConf && cd term-config' C-m
 
