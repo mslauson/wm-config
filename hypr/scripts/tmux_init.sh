fee1d1ab60be447965d71c0dbc423690320fd0d9
Matthew Slauson <matthew@slauson.io>
2023-06-25 14:04:48 -0500

    fix tmux init

--- hypr/scripts/tmux_init.sh
+++ hypr/scripts/tmux_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 WORK="work"
+REST="rest"
 SSH="ssh"
 NOTES="notes"
 CONF="conf"
@@ -9,6 +10,7 @@ OTHER="other"
 tmux start-server
 
 tmux new-session -d -s $WORK
+tmux new-session -d -s $REST
 tmux new-session -d -s $SSH
 tmux new-session -d -s $NOTES
 tmux new-session -d -s $CONF
@@ -19,12 +21,20 @@ tmux new-session -d -s $OTHER
 
 tmux new-window -t $NOTES:0 -n 'notes'
 tmux send-keys -t $NOTES:0 'nvim -c ":Neorg index"' C-m
+tmux select-window -t $NOTES:0 
+
+tmux new-window -t $REST:0 -n 'rest'
+tmux send-keys -t $REST:0 'pSql && nvim .' C-m
+tmux select-window -t $REST:0 
 
 tmux new-window -t $WORK:0 -n 'api'
 tmux send-keys -t $WORK:0 'pApi' C-m
 
 tmux new-window -t $WORK:1 -n 'ui'
 tmux send-keys -t $WORK:1 'pUi' C-m
+
+tmux select-window -t $WORK:0 
+
 
 tmux new-window -t $CONF:0 -n 'nvim'
 tmux send-keys -t $CONF:0 'pConf && cd astro-config' C-m
@@ -34,4 +44,7 @@ tmux send-keys -t $CONF:1 'pConf && cd wm-config' C-m
 
 tmux new-window -t $CONF:2 -n 'term'
 tmux send-keys -t $CONF:2 'pConf && cd term-config' C-m
+
+
+tmux select-window -t $CONF:0 
 
