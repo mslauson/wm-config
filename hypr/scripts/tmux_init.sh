1f9355ed895e77075da191103d8fa042d2ecc8d0
Matthew Slauson <matthew@slauson.io>
2023-07-23 11:31:13 -0500

    fix

--- hypr/scripts/tmux_init.sh
+++ hypr/scripts/tmux_init.sh
@@ -20,31 +20,18 @@ tmux new-session -d -s $OTHER
 # For example, for WORK:
 
 tmux new-window -t $NOTES:0 -n 'notes'
-tmux send-keys -t $NOTES:0 'nvim -c ":Neorg index"' C-m
-tmux select-window -t $NOTES:0 
+tmux select-window -t $NOTES:0
 
 tmux new-window -t $REST:0 -n 'rest'
-tmux send-keys -t $REST:0 'pRest && nvim .' C-m
-tmux select-window -t $REST:0 
+tmux send-keys -t $REST:0 'pRest' C-m
+tmux select-window -t $REST:0
 
 tmux new-window -t $WORK:0 -n 'api'
 tmux send-keys -t $WORK:0 'pApi' C-m
 
-tmux new-window -t $WORK:1 -n 'ui'
-tmux send-keys -t $WORK:1 'pUi' C-m
-
-tmux select-window -t $WORK:0 
-
+tmux select-window -t $WORK:0
 
 tmux new-window -t $CONF:0 -n 'nvim'
-tmux send-keys -t $CONF:0 'pConf && cd astro-config' C-m
+tmux send-keys -t $CONF:0 'pConf' C-m
 
-tmux new-window -t $CONF:1 -n 'wm'
-tmux send-keys -t $CONF:1 'pConf && cd wm-config' C-m
-
-tmux new-window -t $CONF:2 -n 'term'
-tmux send-keys -t $CONF:2 'pConf && cd term-config' C-m
-
-
-tmux select-window -t $CONF:0 
-
+tmux select-window -t $CONF:0
