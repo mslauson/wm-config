a7a6cd37c678ed637c526eea82b30b9f83ceba82
Matthew Slauson <matthew@slauson.io>
2023-08-03 22:40:38 -0500

    add

--- init.sh
+++ init.sh
@@ -19,6 +19,7 @@ rm ~/.config/hypr/workspace.conf
 rm ~/.config/hypr/computer_binds.conf
 
 ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/workspace.conf ~/.config/hypr/workspace.conf
+ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/vars.conf ~/.config/hypr/vars.conf
 ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/computer_binds.conf ~/.config/hypr/computer_binds.conf
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
 for dir in bat btop cava dunst fish gtk-2.0 gtk-3.0 gtk-4.0 hypr wofi ranger scripts starship swaylock waybar wlogout networkmanager-dmenu rofi systemd; do
