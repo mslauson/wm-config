c507e1b2d3e838f09127ad48703acb1da9a677f5
Matthew Slauson <matthew@slauson.io>
2023-09-23 23:51:17 -0500

    id

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+#
 currentPath=/home/mslauson/projects/config/wm-config
 
 echo "Which computer are we on today?" | lolcat
 COMPUTER="$(gum choose desktop thonk devone cancel)"
 echo "Initializing for $COMPUTER"
-# case $COMPUTER in
 #     desktop )
+# case "$COMPUTER" in
 #
 #         break;;
 #     thonk )
@@ -19,14 +21,17 @@ rm ~/.config/hypr/workspace.conf
 rm ~/.config/hypr/vars.conf
 rm ~/.config/hypr/computer_binds.conf
 
-ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/workspace.conf ~/.config/hypr/workspace.conf
-ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/vars.conf ~/.config/hypr/vars.conf
-ln -s /home/mslauson/projects/config/wm-config/hypr/computers/$COMPUTER/computer_binds.conf ~/.config/hypr/computer_binds.conf
+ln -s /home/mslauson/projects/config/wm-config/hypr/computers/"$COMPUTER"/workspace.conf ~/.config/hypr/workspace.conf
+ln -s /home/mslauson/projects/config/wm-config/hypr/computers/"$COMPUTER"/vars.conf ~/.config/hypr/vars.conf
+ln -s /home/mslauson/projects/config/wm-config/hypr/computers/"$COMPUTER"/computer_binds.conf ~/.config/hypr/computer_binds.conf
 # for dir in aconfmgr alacritty autostart BetterDiscord btop cava doom emacs dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr helix i3 kitty Kvantum mako neofetch OpenRGB paru polybar qt5ct qt6ct ranger rofi swappy swaylock waybar wlogout xfce
 for dir in bat btop cava dunst gtk-2.0 gtk-3.0 gtk-4.0 hypr wofi ranger scripts swaylock waybar wlogout networkmanager-dmenu rofi systemd; do
 	rm -rf ~/.config/$dir
 	ln -s "$currentPath"/$dir ~/.config/$dir
 done
+
+rm -rf ~/.config/waybar/config.jsonc
+ln -s /home/mslauson/projects/config/wm-config/waybar/computers/"$COMPUTER"/config.jsonc ~/.config/waybar/config.jsonc
 
 for item in .xprofile .starship.toml; do
 	rm -rf ~/$item
