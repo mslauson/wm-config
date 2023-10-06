1011ef85093e4ff834099a90f07f6bdbfeb30542
Matthew Slauson <matthew@slauson.io>
2023-10-06 08:32:31 -0500

    fix again

--- hypr/scripts/tmux_init.sh
+++ hypr/scripts/tmux_init.sh
@@ -10,22 +10,22 @@ OTHER="other"
 tmux start-server
 
 tmux new-session -d -s $WORK
-tmux new-session -d -s $REST
-tmux new-session -d -s $SSH
-tmux new-session -d -s $NOTES
+# tmux new-session -d -s $REST
+# tmux new-session -d -s $SSH
+# tmux new-session -d -s $NOTES
 tmux new-session -d -s $CONF
 tmux new-session -d -s $OTHER
 
 # Here you can set up windows or panes for each of your sessions.
 # For example, for WORK:
 
-tmux new-window -t $NOTES:0 -n 'notes'
-tmux select-window -t $NOTES:0
+# tmux new-window -t $NOTES:0 -n 'notes'
+# tmux select-window -t $NOTES:0
 
-tmux new-window -t $REST:0 -n 'rest'
-tmux send-keys -t $REST:0 'pRest' C-m
-tmux select-window -t $REST:0
-
+# tmux new-window -t $REST:0 -n 'rest'
+# tmux send-keys -t $REST:0 'pRest' C-m
+# tmux select-window -t $REST:0
+#
 tmux new-window -t $WORK:0 -n 'api'
 tmux send-keys -t $WORK:0 'pApi' C-m
 
