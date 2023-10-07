95a593db92bfe92b2c9392b1fdd19d10f7845e88
Matthew Slauson <matthew@slauson.io>
2023-10-07 12:36:14 -0500

    add goland and init script

--- init.sh
+++ init.sh
@@ -5,18 +5,15 @@ currentPath=/home/mslauson/projects/config/wm-config
 echo "Which computer are we on today?" | lolcat
 COMPUTER="$(gum choose desktop thonk devone cancel)"
 echo "Initializing for $COMPUTER"
-#     desktop )
-# case "$COMPUTER" in
-#
-#         break;;
-#     thonk )
-#         break;;
-#     Cancel )
-#         echo "No config was changed!"
-#         exit
-#         ;;
-# esac
 
+echo "Configuring Custom Desktop Files" | lolcat
+for file in "$currentPath"/desktop/*.desktop; do
+	filename=$(basename "$file")
+	rm -rf "/home/mslauson/.local/share/applications/$filename"
+	ln -s "$file" "/home/mslauson/.local/share/applications/$filename"
+done
+
+echo "Configuring Hypr" | lolcat
 rm ~/.config/hypr/workspace.conf
 rm ~/.config/hypr/vars.conf
 rm ~/.config/hypr/computer_binds.conf
@@ -37,11 +34,13 @@ rm -rf ~/.config/waybar/style.css
 ln -s /home/mslauson/projects/config/wm-config/waybar/computers/"$COMPUTER"/config.jsonc ~/.config/waybar/config.jsonc
 ln -s /home/mslauson/projects/config/wm-config/waybar/computers/"$COMPUTER"/style.css ~/.config/waybar/style.css
 
+echo "Configuring Starship" | lolcat
 for item in .xprofile .starship.toml; do
 	rm -rf ~/$item
 	ln -s "$currentPath"/$item ~/$item
 done
 
+echo "Configuring Hypr Stores And Scripts" | lolcat
 rm -rf /home/mslauson/projects/config/wm-config/hypr/store
 mkdir /home/mslauson/projects/config/wm-config/hypr/store
 touch /home/mslauson/projects/config/wm-config/hypr/store/dynamic_out.txt
